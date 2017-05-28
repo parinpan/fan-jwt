83b019b54d473c595ed9b1b4ffd8eaaf0cbc58cd
M. Fachrin Aulia Nasution <saya@fachrinfan.com>
2017-05-29 03:56:17 +0700

    renew

--- libs/JWTAuth.php
+++ libs/JWTAuth.php
@@ -33,8 +33,9 @@ class JWTAuth
 			);
 		}
 
+		$redirUrl = $jwt ? @$props['redir'] : @$_SERVER['HTTP_REFERER'];
 		header('Content-Type: text/html');
-		header('Refresh: 3; URL=' . ($jwt ? $props['redir'] : $_SERVER['HTTP_REFERER']));
+		header('Refresh: 3; URL=' . $redirUrl;
 
 		return "Please wait, while we are signing you in....";
 	}
